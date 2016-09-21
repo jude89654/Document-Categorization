f9ba981be575f910d874ee3213c2dfd2b75b393f
Jude <jude89654@gmail.com>
2016-09-21 12:48:37 +0800

    initial gui

--- src/com/ust/form/GUI.java
+++ src/com/ust/form/GUI.java
@@ -1,24 +1,30 @@
 package com.ust.form;
 
+import com.ust.main.Main;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by pc1 on 21/09/2016.
  */
 public class GUI extends JFrame {
     private JLabel logoPanel;
-    private JTextArea textArea1;
-    private JTextField textField1;
-    private JButton BROWSEButton;
-    private JTextField textField2;
-    private JButton BROWSEButton1;
+    private JTextArea logsTextArea;
+    private JTextField sourcePathTextField;
+    private JButton sourceBrowseButton;
+    private JTextField outputPathTextField;
+    private JButton outputBrowseButton;
     private JButton RESETButton;
     private JCheckBox trainCheckBox;
     private JCheckBox testCheckBox;
     private JButton STARTButton;
     private JPanel mainPanel;
+    private String welcomeText = "THANK YOU FOR USING THE MEANS SYSTEM";
 
-    public GUI(){
+
+    public GUI() {
         super("DOCUMENT GENRE DETECTOR AND SUBCATEGORIZER");
 
         setContentPane(mainPanel);
@@ -26,11 +32,42 @@ public class GUI extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+
+
+        logsTextArea.setText(welcomeText);
+
+        //actions for the browse button
+        sourceBrowseButton.addActionListener(e -> {
+            String path = getFolderPath();
+            sourcePathTextField.setText(path);
+        });
+        outputBrowseButton.addActionListener(e -> {
+            String path = getFolderPath();
+            outputPathTextField.setText(path);
+        });
+        RESETButton.addActionListener(e -> {
+            logsTextArea.setText("");
+            logsTextArea.setText(welcomeText);
+        });
+
+
+        //listerner for the start button
+
+        STARTButton.addActionListener(e -> {
+            boolean test = testCheckBox.isSelected();
+            boolean train = trainCheckBox.isSelected();
+            if (sourcePathTextField.getText().trim().length() != 0 |
+                    outputPathTextField.getText().trim().length() != 0) {
+                String sourcePath = sourcePathTextField.getText();
+                String destinationPath = outputPathTextField.getText();
+                Main.categorize(new String[]{sourcePath,destinationPath},train,test);
+            }
+        });
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         GUI gui = new GUI();
-         try {
+        try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 System.out.println(info.getName());
                 if ("Metal".equals(info.getName())) {
@@ -40,6 +77,20 @@ public class GUI extends JFrame {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public String getFolderPath() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(false);
+        if (chooser.showDialog(new JFrame(), "SELECT FOLDER") == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile().getPath();
+        } else {
+            System.out.println("YOU MUST SELECT A FOLDER WHERE THE SOURCE DATA COMES FROM");
+            JOptionPane.showMessageDialog(mainPanel, "YOU MUST SELECT A FOLER");
+            //stopProgram();
+            return "";
         }
     }
 }
--- src/com/ust/main/Main.java
+++ src/com/ust/main/Main.java
@@ -2,6 +2,8 @@ package com.ust.main;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 import com.ust.SVM.Test;
@@ -13,7 +15,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.xmlgraphics.image.codec.util.SeekableOutputStream;
 
 /**
- * com.ust.main.Main class of the system
+ * com.ust.categorize.Main class of the system
  */
 public class Main {
     /**
@@ -21,91 +23,118 @@ public class Main {
      */
     static String categorizedFilesPath = "CategorizedDocuments";
 
-    static String projectFolderPath ="project";
+    static String projectFolderPath = "project";
 
-    static String tempFolderPath= "tempFolder";
-
+    static String tempFolderPath = "tempFolder";
 
     static String classNameFile = "class_name.txt";
 
     static String devLabelFile = "dev_label.txt";
 
-    static ArrayList<String> categoryFolderNames = new ArrayList<>();
+    static Map<Integer, String> categoryFolderNames = new HashMap<>();
 
     /**
-     * com.ust.main.Main method of the program
+     * com.ust.categorize Main method of the program
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void categorize(String[] args, boolean train, boolean test) {
 
 
+        if (args.length == 2) {
+            projectFolderPath = args[0];
+            categorizedFilesPath = args[1] + File.separator + categorizedFilesPath;
+        }
         // static String[] categories
+
+        //Copy the class_name file
+        copyClassFile();
+
+        //Copy the dev_label file
+        copyDevFile();
+
+        //get categories
+        getTrainingFolderNames();
         try {
-
-            //Method used to create an OCR File of the test and training data
-            createOCRFiles();
-            //Copy the class_name file
-            copyClassFile();
-
-            //Copy the dev_label file
-            copyDevFile();
-
-            //get categories
-            getTrainingFolderNames();
-
-            //Start Training all data
-            Train.startTraining();
-
-            //Start Classifying all the testData
-            Test.StartTesting();
-
-            //Delete the Categorized documents folder
-            FileUtils.deleteDirectory(new File(categorizedFilesPath));
-            ////Create the root folder and the subfolders for the categorized files
-            initializeFolders();
-
-            //create an array of the files and move it to its respective folders using the result.txt
-            //get all png and jpeg files
-            File[] originalFiles = new File("project/dev").listFiles(new PictureFileFilter());
-            ArrayList<Integer> results = getResults(new File("result.txt"));
-
-            //copy the files.
-            moveCategorizedFiles(originalFiles, results);
-
-
-        } catch (IOException io) {
-            System.out.println("ERROR IN INITIALIZING FILES");
-            io.printStackTrace();
-        } catch (InvalidInputDataException e) {
+            if (train) {
+                //start copying training files
+                copyTrainingFiles();
+                //Start Training all data
+                Train.startTraining();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
+
+        try {
+            //Start Classifying all the testData
+            if (test) {
+                //Method used to create an OCR File of the test and training data
+                createTestOCRFiles();
+
+                Test.StartTesting();
+
+                //Delete the Categorized documents folder
+                FileUtils.deleteDirectory(new File(categorizedFilesPath));
+                ////Create the root folder and the subfolders for the categorized files
+                initializeFolders();
+
+                //create an array of the files and move it to its respective folders using the result.txt
+                //get all png and jpeg files
+                File[] originalFiles = new File("project/dev").listFiles(new PictureFileFilter());
+                ArrayList<Integer> results = getResults(new File("result.txt"));
+
+                //copy the files.
+                moveCategorizedFiles(originalFiles, results);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
     /**
      * Method to create OCR Files on the startTraining and dev folder
      */
-    public static void createOCRFiles(){
+    public static void createTestOCRFiles() {
         try {
             System.out.println("CREATING OCR FOR TEST FILES");
-            FileUtilities.createOCRFile(new File(projectFolderPath +"/dev" ), new File(tempFolderPath+"/dev"));
-            System.out.println("CREATING OCR FOR TRAINING FILES");
-            FileUtilities.createOCRFile(new File(projectFolderPath +"/train"), new File(tempFolderPath+"/train"));
+            FileUtilities.createOCRFile(new File(projectFolderPath + "/dev"), new File(tempFolderPath + "/dev"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void  copyDevFile(){
-          FileUtilities.copyDevLabelFile(new File(projectFolderPath+File.separator+devLabelFile), new File(tempFolderPath+File.separator+devLabelFile));
+    /**
+     * method to copy text files for training
+     */
+    public static void copyTrainingFiles() {
+        System.out.println("COPYING TRAINING FILES");
+        try {
+            FileUtilities.createOCRFile(new File(projectFolderPath + "/train"), new File(tempFolderPath + "/train"));
+        } catch (IOException e) {
+            System.out.println("ERROR IN COPYING TRAINING FILES");
+            e.printStackTrace();
+        }
     }
 
-    public static void copyClassFile(){
+    public static void copyDevFile() {
+        System.out.println("COPYING DEV FILE");
+        FileUtilities.copyDevLabelFile(new File(projectFolderPath + File.separator + devLabelFile), new File(tempFolderPath + File.separator + devLabelFile));
+        System.out.println("DEV FILE COPIED");
+    }
+
+    public static void copyClassFile() {
         try {
-            FileUtils.copyFile(new File(projectFolderPath+File.separator+classNameFile), new File(tempFolderPath+File.separator+classNameFile));
+            System.out.println("COPYING CLASS NAMES FILE");
+            FileUtils.copyFile(new File(projectFolderPath + File.separator + classNameFile), new File(tempFolderPath + File.separator + classNameFile));
+            System.out.println("CLASS NAME FILE COPIED");
         } catch (IOException e) {
+            System.out.println("ERROR IN COPYING CLASS FILE");
             e.printStackTrace();
         }
     }
@@ -114,13 +143,15 @@ public class Main {
     /**
      * Method for moving the categorized files based on the results.txt
      *
-     * @param categorizedfile Array of files
+     * @param categorizedFile Array of files
      * @param results
      */
-    public static void moveCategorizedFiles(File[] categorizedfile, ArrayList<Integer> results) {
+    public static void moveCategorizedFiles(File[] categorizedFile, ArrayList<Integer> results) {
+        System.out.println("MOVING FILES");
         for (int index = 0; index < results.size(); index++) {
             try {
-                FileUtils.copyFile(categorizedfile[index], new File(categorizedFilesPath + "/" + categoryFolderNames.get(results.get(index)) + categorizedfile[index].getName()));
+                System.out.println("MOVING FILE" + categorizedFile[index].getName());
+                FileUtils.copyFile(categorizedFile[index], new File(categorizedFilesPath + "/" + categoryFolderNames.get(results.get(index)) + categorizedFile[index].getName()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -161,31 +192,34 @@ public class Main {
 
         //folders for the categorized documents root directory
 
-        for (String currentFolderName : categoryFolderNames) {
-            File file = new File("CategorizedDocuments/" + currentFolderName);
+        for (String currentFolderName : categoryFolderNames.values()) {
+            File file = new File(categorizedFilesPath + File.separator + currentFolderName);
             file.mkdir();
         }
+        System.out.println("FOLDERS CREATED");
 
     }
 
-    public static void getTrainingFolderNames(){
+    public static void getTrainingFolderNames() {
         //ArrayList<String> pathNames = new ArrayList<>();
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(projectFolderPath+File.separator+classNameFile));
+            BufferedReader br = new BufferedReader(new FileReader(projectFolderPath + File.separator + classNameFile));
             String line;
-            while((line=br.readLine())!=null){
-                String path =line.split(" ")[1];
-                path=path.replace(".",File.separator);
-                path+=File.separator;
-                categoryFolderNames.add(path);
+            while ((line = br.readLine()) != null) {
+                String path = line.split(" ")[1];
+                path = path.replace(".", File.separator);
+                path += File.separator;
+
+                categoryFolderNames.put(Integer.parseInt(line.split(" ")[0]), path);
             }
-            categoryFolderNames.add("others"+File.separator);
+            categoryFolderNames.put(categoryFolderNames.size(), "others" + File.separator);
 
         } catch (FileNotFoundException e) {
             System.out.println("CLASS FILE NOT FOUND");
             e.printStackTrace();
         } catch (IOException e) {
+            System.out.println("ERROR IN CLASS FILE");
             e.printStackTrace();
         }
 
